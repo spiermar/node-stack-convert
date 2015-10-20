70adba4d22d81e9845127415a1d8a54a93190049
Martin Spier <spiermar@gmail.com>
2015-10-20 12:53:54 -0700

    Parsing folded stacks.

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
+var fs = require('fs');
 var program = require('commander');
 
+function Node(name) {
+    this.name = name;
+    this.value = 0;
+    this.children = {};
+}
+
+Node.prototype.add = function(frames, value) {
+  this.value += value;
+  if(frames && frames.length > 0) {
+    var head = frames[0];
+    var child = this.children[head];
+    if(!child) {
+      child = new Node(head);
+      this.children[head] = child;
+    }
+    frames.splice(0, 1);
+    child.add(frames, value);
+  }
+}
+
 program
-  .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .version('0.1.0')
+  .option('-i, --input <filename>', 'Select input file.', null)
+  .option('-o, --output <filename>', 'Select output file.', null)
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+if (!program.input) program.help(function(t) {
+  return '\n  Error: -i --input <filename> is required.\n' + t;
+});
+
+fs.readFile(program.input, 'utf8', function (err, data) {
+  if (err) throw err;
+  var root = new Node('root');
+  data.split("\n").map(function (val) {
+    var regex = /(.*) (.*)/g;
+    var matches = regex.exec(val);
+    if(matches) root.add(matches[1].split(";"), parseInt(matches[2]));
+  });
+  console.log(JSON.stringify(root, null, 2));
+});
